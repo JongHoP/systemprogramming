7fef55b2bd518c3ab8f0b673269808e10a46ca4d
JongHoP <ysj7376@gmail.com>
2021-10-21 01:38:02 -0700

    assert complete

--- lab2/assert/assert_test.c
+++ lab2/assert/assert_test.c
@@ -1,1 +1,24 @@
-##
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+
+void foo(int num)
+{
+	assert(((num>=0)&&(num<=100)));
+
+	printf("foo: num = %d\n",num);
+}
+
+main(int argc, char *argv[])
+{
+	int num;
+
+	if(argc<2){
+	fprintf(stderr, "Usage:aseert_test aNumber\n(0<= aNumber<= 100)\n");
+exit(1);
+}
+num =atoi(argv[1]);
+foo(num);
+}
+
+
