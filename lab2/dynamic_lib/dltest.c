f7045275c6b2d734385035714188fc579dfef867
Seonhyun Kim <sun12334567@gmail.com>
2021-10-22 00:50:23 +0900

    dynamic lib success

--- lab2/dynamic_lib/dltest.c
+++ lab2/dynamic_lib/dltest.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dlfcn.h>
 
 int main(){
 	
 	void *handle;
-	int (*add)(int, int), (*substract)(int, int), (*divide)(int, int), (*multiply)(int, int)
+	int (*add)(int, int), (*subtract)(int, int), (*divide)(int, int), (*multiply)(int, int);
 	char *error;
 
 	handle = dlopen ("./sources/libtest.so", RTLD_LAZY);
@@ -20,7 +21,7 @@ int main(){
 		exit(1);
 	}
 	
-	substract = dlsym(handle, "substract");
+	subtract = dlsym(handle, "subtract");
 
 	if((error = dlerror()) != NULL) {
 		fprintf(stderr, "%s", error);
@@ -35,15 +36,17 @@ int main(){
 	}
 	
 	multiply = dlsym(handle, "multiply");
-	if(error = dlerror()) != NULL) {
+	if((error = dlerror()) != NULL) {
 
 		fprintf(stderr, "%s", error);
 		exit(1);
 	}
 	
-	printf("ADD(8,4): %s"); (*add)(8,4);
-	printf("SUBTRACT(8,4): "); subtract(8,4);
-	printf("MULTIPLY(8,4): "); multiply(8,4);
-	printf("DIVIDE(8,4): "); divide(8,4);
-
+	printf("ADD(8,4): "); (*add)(8,4);
+	printf("SUBTRACT(8,4): "); (*subtract)(8,4);
+	printf("MULTIPLY(8,4): "); (*multiply)(8,4);
+	printf("DIVIDE(8,4): ");(*divide)(8,4);
+	printf("\n");
+	dlclose(handle);
+}
 
