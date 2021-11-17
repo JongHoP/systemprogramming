56875dc47aaefa8afb969bcedd86e18d5db63f7c
JongHoP <ysj7376@gmail.com>
2021-11-16 22:04:57 -0800

    pipe

--- lab3/No.2 and 3/selfpipe.c
+++ lab3/No.2 and 3/selfpipe.c
@@ -11,10 +11,13 @@ int main()
 	char buf[MSGSIZE];
 	int p[2],i;
 
-	if(pipe(p)==-1}{
+	if(pipe(p)==-1){
 		perror("pipe call failed");
 		exit(1);
 	}
+
+for(i=0;i<2;i++)
+	write(p[1],msg[i],MSGSIZE);
 
 for(i=0;i<2;i++){
 	read(p[0],buf,MSGSIZE);
